fbb254592cc8ac3ff8b2ae0c3084b614801765d6
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-01-23 07:52:34 -0500

    import blogPost model into routes

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
+const BlogPost = require('../models/blogPost');
+
+// Server Routes
+router.get('/api', (req, res) => {
+
+    BlogPost.find({  })
+        .then((data) => {
+            console.log('Data', data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log('error', error)
+        });
+})
+
+router.get('/api/name', (req, res) => {
+    const data = {
+        username: 'wielfried',
+        age: 40
+    };
+    res.json(data)
+})
 module.exports = router;
