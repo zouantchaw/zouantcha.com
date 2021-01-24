9ac1faf44b9baa0d3b32fd4c39f7ff59f4a4a3c5
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-01-24 08:10:41 -0500

    link css to app|

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import { Button } from '@material-ui/core';
+
+import './App.css'
 
 
 class App extends React.Component {
@@ -102,7 +103,7 @@ class App extends React.Component {
             </textarea>
           </div>
 
-          <Button color="secondary">Submit</Button>
+          <button>Submit</button>
         </form>
 
         <div className="blog">
