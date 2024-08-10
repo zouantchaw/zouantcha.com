7fe2f928ece03541d6c1e21a6f8d6d8e2afe81ba
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2024-08-10 14:58:10 -0400

    Add async script for Seline tracking on layout and document

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,13 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <head>
+        <script
+          async
+          src="https://cdn.seline.so/seline.js"
+          data-token="dfbe2e89cb93290"
+        ></script>
+      </head>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,12 +23,6 @@ export default function Document() {
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
-        <Script
-          src="https://cdn.seline.so/seline.js"
-          data-token="dfbe2e89cb93290"
-          strategy="afterInteractive"
-          async
-        />
       </Head>
       <body>
         <Main />
